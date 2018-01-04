fd936cba22535187dbe9fb8e361baf7a9955d308
IsaacEE <301882338>
2018-01-05 01:44:18 +0200

    Is valid function in board

--- src/Board.java
+++ src/Board.java
@@ -4,15 +4,32 @@
 public class Board {
 
 	CellState[][] _actualBoard;
+	int _size;
 
-	public Board(CellState[][] actualBoard){
+	public Board(CellState[][] actualBoard, int size){
 		_actualBoard=actualBoard;
+		_size=size;
 	}
 
-	public boolean isValid(Step step){
-		if(_actualBoard[step.GetI()][step.GetJ()]!= CellState.EMPTY)
+	public boolean IsValid(Step step){
+		int i=step.GetI();
+		int j=step.GetJ();
+		if(i<0||i==_size||j<0||j==_size||_actualBoard[i][j]!= CellState.EMPTY)
 			return false;
-
+		if((i>0&&_actualBoard[i-1][j]!=CellState.EMPTY) //up != Empty
+				   ||(i<(_size-1)&&_actualBoard[i+1][j]!=CellState.EMPTY) //down != Empty
+				   ||(j>0&&_actualBoard[i][j-1]!=CellState.EMPTY) //left != Empty
+				   ||(j<(_size-1)&&_actualBoard[i][j+1]!=CellState.EMPTY)//right != Empty
+				   ||(i>0&&j>0&&_actualBoard[i-1][j-1]!=CellState.EMPTY) // upleft != Empty
+				   ||(i>0&&j<(_size-1)&&_actualBoard[i-1][j+1]!=CellState.EMPTY)// upright != Empty
+				   ||(i<(_size-1)&&j<(_size-1)&&_actualBoard[i+1][j+1]!=CellState.EMPTY)// downright != Empty
+				   ||(i<(_size-1)&&j>0&&_actualBoard[i+1][j-1]!=CellState.EMPTY)) //downlefr != Empty
+		{
+			return true;
+		}
+		return false;
+	}
+	public Board MakeStep(Step step){
 
 	}
 }
